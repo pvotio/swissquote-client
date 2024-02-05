768518d5cf078d9ab055e716175e892f6c05a632
Ali Moghimi <mghmiali@gmail.com>
2024-02-05 14:04:56 +0000

    feat: init commit

---
+++ config/settings.py
@@ -0,0 +1,11 @@
+from decouple import config
+
+LOG_LEVEL = config("LOG_LEVEL", default="INFO")
+OUTPUT_TABLE = config("OUTPUT_TABLE")
+INSERTER_MAX_RETRIES = config("INSERTER_MAX_RETRIES", default=3, cast=int)
+REQUEST_MAX_RETRIES = config("REQUEST_MAX_RETRIES", default=3, cast=int)
+REQUEST_BACKOFF_FACTOR = config("REQUEST_BACKOFF_FACTOR", default=2, cast=float)
+MSSQL_SERVER = config("MSSQL_SERVER")
+MSSQL_DATABASE = config("MSSQL_DATABASE")
+MSSQL_USERNAME = config("MSSQL_USERNAME")
+MSSQL_PASSWORD = config("MSSQL_PASSWORD")
