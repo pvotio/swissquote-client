29f716b55efff34b22eb4a98e86773419379c197
Ali Moghimi <mghmiali@gmail.com>
2024-02-06 15:28:09 +0000

    feat: add new endpoints

--- swissquote/client.py
+++ swissquote/client.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional, Union
 
 from swissquote import request
 
@@ -9,8 +9,8 @@ class SwissQuote:
 
     def __init__(self, token: str, max_retries: int, backoff_factor: float) -> None:
         self.req = request.init_session(token, max_retries, backoff_factor)
-        
-    def request(self, url):
+
+    def request(self, url: str) -> Union[list, dict]:
         resp = self.req.get(url)
         resp.raise_for_status()
         return resp.json()
@@ -19,18 +19,30 @@ class SwissQuote:
         url = self.BASE_URL + "clients"
         return self.request(url)
 
-    def get_positions(self, clientid: Union[int, str]):
+    def get_positions(self, clientid: Union[int, str]) -> dict:
         url = self.BASE_URL + f"clients/{clientid}/positions"
         return self.request(url)
 
-    def get_transactions(self, clientid: Union[int, str]):
+    def get_transactions(
+        self, clientid: Union[int, str], page: Optional[int] = None
+    ) -> dict:
         url = self.BASE_URL + f"clients/{clientid}/transactions"
+        if page:
+            url = url + f"?page={page}"
+
         return self.request(url)
 
     def get_buyingpower(self, clientid: Union[int, str], currency: str) -> dict:
         url = self.BASE_URL + f"clients/{clientid}/buyingPower/{currency}"
         return self.request(url)
 
-    def get_lists(self) -> dict:
-        url = self.BASE_URL + f"lists/"
+    def get_rates(self, date: Optional[str]) -> dict:
+        url = self.BASE_URL + "clients/rates"
+        if date:
+            url = url + f"?date={date}"
+
+        return self.request(url)
+
+    def get_staticlists(self) -> dict:
+        url = self.BASE_URL + "lists/"
         return self.request(url)
